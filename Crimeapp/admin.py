e198f0a4de65e171cbe4558849382f9f92bbce23
Akhil Satheesh <akhilpalackal2003@gmail.com>
2024-10-20 10:30:24 +0530

    commit done

---
+++ Crimeapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(register_form)
+admin.site.register(complaint)
+admin.site.register(area)
+admin.site.register(complaint_details)
+admin.site.register(Missing)
+admin.site.register(addcriminalls)
+admin.site.register(branches)
+admin.site.register(chatregg)
+admin.site.register(fileupload)
---
+++ Crimeapp/urls.py
@@ -0,0 +1,41 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.Run_Here, name="Run_Here"),
+    path("Run_Here", views.Run_Here, name="Run_Here"),
+    path("contact", views.contact, name="contact"),
+    path("finishh", views.finishh, name="finishh"),
+    path("crimemissing", views.crimemissing, name="crimemissing"),
+    path("crimemissinglist", views.crimemissinglist, name="crimemissinglist"),
+    path("publiccomplaint", views.publiccomplaint, name="publiccomplaint"),
+    path("publiccomp", views.publiccomp, name="publiccomp"),
+    path("logincheck", views.logincheck, name="logincheck"),
+    path("branchlogin", views.branchlogin, name="branchlogin"),
+    path("branch", views.branch, name="branch"),
+    path("admincheck", views.admincheck, name="admincheck"),
+    path("register", views.register, name="register"),
+    path("areareg", views.areareg, name="areareg"),
+    path("areas", views.areas, name="areas"),
+    path("index", views.index, name="index"),
+    path("Add_Complaint", views.Add_Complaint, name="Add_Complaint"),
+    path("Complaintstatus", views.Complaintstatus, name="Complaintstatus"),
+    path("AddMissing", views.AddMissing, name="AddMissing"),
+    path("missingstatus", views.missingstatus, name="missingstatus"),
+    path("policehome", views.policehome, name="policehome"),
+    path("complaint", views.ccomplaint, name="complaint"),
+    path("Missing_details", views.Missing_details, name="Missing_details"),
+    path("criminalview", views.criminalview, name="criminalview"),
+    path("chatpolice", views.chatpolice, name="chatpolice"),
+    path("addcriminals", views.addcriminals, name="addcriminals"),
+    path("viewcomp", views.viewcomp, name="viewcomp"),
+    path("viewcomp2", views.viewcomp2, name="viewcomp2"),
+    path("viewcomp3", views.viewcomp3, name="viewcomp3"),
+    path("addcrime", views.addcrime, name="addcrime"),
+    path("publiccompl", views.publiccompl, name="publiccompl"),
+    path("chatpolice2", views.chatpolice2, name="chatpolice2"),
+    path("chatreg", views.chatreg, name="chatreg"),
+    path("finish", views.finish, name="finish"),
+
+  
+]
